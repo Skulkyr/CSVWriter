301a11500e28e64710eec71698ef2844382cc7f7
Alexey <skulkyr20@gmail.com>
2024-11-15 14:16:19 +0300

    rebase to gradle

--- src/test/java/org/pogonin/RecursiveCSVWriterTest.java
+++ src/test/java/org/pogonin/RecursiveCSVWriterTest.java
@@ -19,7 +19,7 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class RecursiveCSVWriterTest {
+class RecursiveCSVWriterTest {
 
     private CSVWriter converter;
 
